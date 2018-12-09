34fb640b59d1859e2c1b01ab46a12da9d2110d1d
mwfister <mwfister@gmail.com>
2018-12-09 17:45:18 +1100

    Batch mongo queries

--- database/mdb.js
+++ database/mdb.js
@@ -1,9 +1,14 @@
+const { orderedFor } = require('../lib/util')
+
 module.exports = mPool => {
   return {
-    getCounts(user, countsField) {
+    getUsersByIds(userIds) {
       return mPool.collection('users')
-        .findOne({ userId: user.id })
-        .then(userCollection => userCollection[countsField])
+        .find({ userId: { $in: userIds } })
+        .toArray()
+        .then(rows => {
+          return orderedFor(rows, userIds, 'userId')
+        })
     },
   }
 }
--- database/pgdb.js
+++ database/pgdb.js
@@ -1,22 +1,6 @@
-const humps = require('humps')
-const _ = require('lodash')
+const { orderedFor } = require('../lib/util')
 
 module.exports = pgPool => {
-  const orderedFor = (rows, collection, field, singleObject = false) => {
-    const data = humps.camelizeKeys(rows)
-    const inGroupsOfField = _.groupBy(data, field)
-    /** Check if collection exists in rows
-        Match collection and row by id field
-    */
-    return collection.map(el => {
-      const elementArray = inGroupsOfField[el]
-      if (elementArray) {
-        return singleObject ? elementArray[0] : elementArray
-      }
-      return singleObject ? {} : []
-    })
-  }
-
   return {
     getUsersByApiKeys(apiKeys) {
       return pgPool.query(`
